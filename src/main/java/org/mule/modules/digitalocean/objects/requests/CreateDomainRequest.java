b3e14a1fd77918f717ed0348fb95fb633ed36e80
Bryan Mack <bryangm@gmail.com>
2015-06-30 16:26:28 -0400

    added request objects

---
+++ src/main/java/org/mule/modules/digitalocean/objects/requests/CreateDomainRequest.java
@@ -0,0 +1,42 @@
+package org.mule.modules.digitalocean.objects.requests;
+
+import java.io.Serializable;
+
+import com.google.gson.annotations.SerializedName;
+
+public class CreateDomainRequest implements Serializable {
+	private static final long serialVersionUID = -1L;
+	
+	private String name;
+	
+	@SerializedName("ip_address")
+	private String ipAddress;
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the ipAddress
+	 */
+	public String getIpAddress() {
+		return ipAddress;
+	}
+
+	/**
+	 * @param ipAddress the ipAddress to set
+	 */
+	public void setIpAddress(String ipAddress) {
+		this.ipAddress = ipAddress;
+	}
+}
---
+++ src/main/java/org/mule/modules/digitalocean/objects/requests/UpdateDomainRecordRequest.java
@@ -0,0 +1,103 @@
+package org.mule.modules.digitalocean.objects.requests;
+
+import java.io.Serializable;
+
+public class UpdateDomainRecordRequest implements Serializable {
+	private static final long serialVersionUID = -1L;
+	
+	private String type;
+	
+	private String name;
+	
+	private String data;
+	
+	private Integer priority;
+	
+	private Integer port;
+	
+	private Integer weight;
+
+	/**
+	 * @return the type
+	 */
+	public String getType() {
+		return type;
+	}
+
+	/**
+	 * @param type the type to set
+	 */
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the data
+	 */
+	public String getData() {
+		return data;
+	}
+
+	/**
+	 * @param data the data to set
+	 */
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	/**
+	 * @return the priority
+	 */
+	public Integer getPriority() {
+		return priority;
+	}
+
+	/**
+	 * @param priority the priority to set
+	 */
+	public void setPriority(Integer priority) {
+		this.priority = priority;
+	}
+
+	/**
+	 * @return the port
+	 */
+	public Integer getPort() {
+		return port;
+	}
+
+	/**
+	 * @param port the port to set
+	 */
+	public void setPort(Integer port) {
+		this.port = port;
+	}
+
+	/**
+	 * @return the weight
+	 */
+	public Integer getWeight() {
+		return weight;
+	}
+
+	/**
+	 * @param weight the weight to set
+	 */
+	public void setWeight(Integer weight) {
+		this.weight = weight;
+	}
+}
